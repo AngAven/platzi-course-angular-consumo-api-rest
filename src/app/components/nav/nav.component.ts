6f6527a2f1d94374c8679d4359d3563646428298
Angel <angel.aven.c@gmail.com>
2023-10-11 17:41:37 -0600

    Login desde la barra de navegación

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { StoreService } from '../../services/store.service'
+import {AuthService} from "../../services/auth.service";
+import {User} from "../../models/user.model";
 
 @Component({
   selector: 'app-nav',
@@ -9,11 +11,14 @@ import { StoreService } from '../../services/store.service'
 })
 export class NavComponent implements OnInit {
 
+  token = ''
+  profile: User | null = null
   activeMenu = false;
   counter = 0;
 
   constructor(
-    private storeService: StoreService
+    private storeService: StoreService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
@@ -26,4 +31,19 @@ export class NavComponent implements OnInit {
     this.activeMenu = !this.activeMenu;
   }
 
+  login() {
+    this.authService.login('angelin1@gmail.com','12345')
+      .subscribe(rta => {
+        this.token = rta.access_token
+        console.log('rta login => ', rta)
+        this.getprofile()
+      })
+  }
+
+  getprofile(){
+    this.authService.profile(this.token)
+    .subscribe(user => {
+      this.profile = user
+    })
+  }
 }
