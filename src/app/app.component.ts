39952c324ec937c7b20854ab4c0e45c6fda6ef65
Angel <angel.aven.c@gmail.com>
2023-10-11 23:07:45 -0600

    Subida de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter} from '@angular/core';
 
 import {AuthService} from './services/auth.service'
 import {UsersService} from './services/users.service'
@@ -10,9 +10,10 @@ import {FilesService} from "./services/files.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = '';
+  imgRTA = ''
   showImg = true;
   token = ''
+  statusDetail: 'loading' | 'succes' | 'init' | 'error' = 'init'
 
   constructor(
     private authService: AuthService,
@@ -29,36 +30,51 @@ export class AppComponent {
     this.showImg = !this.showImg;
   }
 
-  createUser(){
+  createUser() {
     this.usersService.create({
-        "email": "angelin1@gmail.com",
-        "name": "angel",
-        "password": "12345",
-        "role": "admin",
-        "avatar": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jYkeFOZmc9yTz6xKUji13wAAAA%26pid%3DApi&f=1&ipt=f6708a44505a8e3d3cd3534f2039869704fd7722548bc575f0f115e3126eded8&ipo=images"
+      "email": "angelin1@gmail.com",
+      "name": "angel",
+      "password": "12345",
+      "role": "admin",
+      "avatar": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jYkeFOZmc9yTz6xKUji13wAAAA%26pid%3DApi&f=1&ipt=f6708a44505a8e3d3cd3534f2039869704fd7722548bc575f0f115e3126eded8&ipo=images"
+    })
+      .subscribe(rta => {
+        console.log('rta create user=> ', rta)
       })
-    .subscribe(rta => {
-      console.log('rta create user=> ', rta)
-    })
   }
 
-  login(){
-    this.authService.login('angelin1@gmail.com','12345')
-    .subscribe(rta => {
-      console.log('rta login => ', rta.access_token)
-      this.token = rta.access_token
-    })
+  login() {
+    this.authService.login('angelin1@gmail.com', '12345')
+      .subscribe(rta => {
+        console.log('rta login => ', rta.access_token)
+        this.token = rta.access_token
+      })
   }
 
-  getProfile(){
+  getProfile() {
     this.authService.profile()
-    .subscribe(profile => {
-      console.log('profile => ', profile)
-    })
+      .subscribe(profile => {
+        console.log('profile => ', profile)
+      })
   }
 
-  downloadPDF(){
+  downloadPDF() {
     return this.filesService.getFile('my-pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
-    .subscribe()
+      .subscribe()
+  }
+
+  onUpload(event: Event) {
+    this.statusDetail = 'loading'
+    const element = event.target as HTMLInputElement
+    const file = element.files?.item(0)
+
+    if (file) {
+      this.filesService.uploadFile(file)
+        .subscribe(rta => {
+          this.imgRTA = rta.location
+          this.statusDetail = 'succes'
+        })
+    }
+
   }
 }
--- src/app/services/files.service.ts
+++ src/app/services/files.service.ts
@@ -4,10 +4,19 @@ import {HttpClient} from "@angular/common/http";
 import {saveAs} from "file-saver";
 import {tap, map} from "rxjs/operators";
 
+import {environment} from '../../environments/environment'
+
+interface File {
+  originalname: string;
+  filename: string;
+  location: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FilesService {
+  private apiUrl = `${environment.API_URL}/api/v1/files`
 
   constructor(
     private httpClient: HttpClient
@@ -22,5 +31,15 @@ export class FilesService {
       }),
       map(() => true)
     )
+  }
+
+  uploadFile(file: Blob){
+    const dto = new FormData()
+    dto.append('file', file)
+    return this.httpClient.post<File>(`${this.apiUrl}/upload`, dto, {
+      // headers: {
+      //   'Content-Type': 'multipart/form-data'
+      // }
+    })
   }
 }
