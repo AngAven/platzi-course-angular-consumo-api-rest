768d4dd190f2b34fe7338ead6474d13d82ce3bb3
Angel <angel.aven.c@gmail.com>
2023-10-11 19:58:22 -0600

    Usando interceptors para peticiones específicas

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import {catchError, map, retry} from "rxjs/operators";
 import {throwError} from "rxjs/";
+import {checkTime} from "../interceptors/time.interceptor";
 
 import {Product, CreateProductDTO, UpdateProductDTO} from './../models/product.model';
 import {environment} from '../../environments/environment'
@@ -23,7 +24,7 @@ export class ProductsService {
       params = params.set('offset', offset)
     }
 
-    return this.http.get<Product[]>( this.apiUrl, {params})
+    return this.http.get<Product[]>( this.apiUrl, {params, context: checkTime()})
     .pipe(
       retry(3),
       map(products => products.map(item => {
